648b978a625c1309fe658993a55a428d046e2ebd
bruno <bruno.marelli@marelli.com>
2021-04-27 12:11:59 +0200

    Enabled OTA feature.

---
+++ MouseTrap.ino
@@ -0,0 +1,36 @@
+#include <WiFi.h>
+
+#include "credentials.h"
+#include "OTA.h"
+
+const char *ssid     = mySSID;
+const char *password = myPASSWORD;
+
+void setup() {
+  // Start serial port for debugging
+  Serial.begin(115200);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Print local IP address
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Setup OTA
+  ArduinoOTA.setHostname("MouseTrap");
+  otaSetup();
+
+}
+
+void loop() {
+  // Check OTA
+  ArduinoOTA.handle();
+}
---
+++ credentials.h
@@ -0,0 +1,2 @@
+#define mySSID "Vodafone-A76077447"
+#define myPASSWORD "9w2hl39lwmevzuvx"
